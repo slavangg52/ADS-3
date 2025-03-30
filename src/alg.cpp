68a4596171399e80d89fd3aea6c6d9ee4b61bafd
slavangg52 <slavatop0203@gmail.com>
2025-03-30 22:07:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,59 @@
 // Copyright 2025 NNTU-CS
+#include <iostream>
 #include <cstdint>
-#include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+    uint64_t maxVal = num;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        if (num > maxVal) {
+            maxVal = num;
+        }
+    }
+    return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned int length = 1;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        ++length;
+    }
+    return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+    unsigned int bestNumber = 0;
+    *maxlen = 0;
+    for (uint64_t i = lbound; i <= rbound; ++i) {
+        unsigned int currentLen = collatzLen(i);
+        if (currentLen > *maxlen) {
+            *maxlen = currentLen;
+            bestNumber = i;
+        }
+    }
+    return bestNumber;
+}
+
+int main() {
+    unsigned int maxLen;
+    uint64_t lbound = 2;
+    uint64_t rbound = 1000000;
+
+    unsigned int bestNumber = seqCollatz(&maxLen, lbound, rbound);
+    uint64_t maxValue = collatzMaxValue(bestNumber);
+
+    std::cout << "Число, формирующее самую длинную последовательность: " << bestNumber << std::endl;
+    std::cout << "Длина последовательности: " << maxLen << std::endl;
+    std::cout << "Максимальное значение в последовательности: " << maxValue << std::endl;
+
+    return 0;
 }
